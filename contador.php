0349f0f7e1e469936ecdcc08e4da071ba562c214
WM-HTML <panabonilla2@gmail.com>
2025-08-22 16:52:46 -0600

    Delete contador.php

--- contador.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Contador de visitas
-$file = 'contador.txt'; // Tu archivo contador
-$contador = file_exists($file) ? (int)file_get_contents($file) : 0;
-$contador++;
-file_put_contents($file, $contador);
-?>
